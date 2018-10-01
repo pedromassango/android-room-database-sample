b4eddad20670fba79470fb37385e415558a7c667
pedromassango <pedromassango.developer@gmail.com>
2018-10-01 19:44:07 +0100

    updated to use Paging library

--- app/src/main/java/com/pedromassango/androidroomdatabasesample/WordsRecyclerAdapter.kt
+++ app/src/main/java/com/pedromassango/androidroomdatabasesample/WordsRecyclerAdapter.kt
@@ -3,6 +3,9 @@ package com.pedromassango.androidroomdatabasesample
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.paging.PagedList
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.pedromassango.androidroomdatabasesample.data.Phrase
 import kotlinx.android.synthetic.main.row_phrase.view.*
@@ -22,25 +25,37 @@ class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
 /**
  * This is the Phrase adapter
  */
-class WordsRecyclerAdapter(private val data: ArrayList<Phrase> = arrayListOf()): RecyclerView.Adapter<MyViewHolder>(){
+class WordsRecyclerAdapter: PagedListAdapter<Phrase, MyViewHolder>(DIFF_CALLBACK){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.row_phrase, parent, false)
         return MyViewHolder(v)
     }
 
-    override fun getItemCount() = data.count()
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val phrase = getItem(position)
 
-    override fun onBindViewHolder(holder: MyViewHolder, index: Int) = holder.bindView(data[index])
-
-    fun add(phrase: Phrase){
-        data.add(phrase)
-        notifyDataSetChanged()
+        // we need to check if the item is null, because the tem can be null
+        when(phrase != null){
+            true -> holder.bindView( phrase!!)
+            false -> holder.bindView( Phrase(" "))
+        }
     }
 
-    fun add(phrases: List<Phrase>){
-        data.clear()
-        data.addAll(phrases)
-        notifyDataSetChanged()
+    companion object {
+
+
+        private val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Phrase>(){
+            override fun areItemsTheSame(oldItem: Phrase,
+                                         newItem: Phrase): Boolean =
+                    oldItem.phrase.equals( newItem.phrase, true)
+
+
+            override fun areContentsTheSame(oldItem: Phrase,
+                                            newItem: Phrase): Boolean =
+                    oldItem.phrase == newItem.phrase
+        }
+
+
     }
 }
